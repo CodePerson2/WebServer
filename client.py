cf44dbf509be5ea1e7cd25a6633595eef44ee285
ramishk <ramish_khan@sfu.ca>
2021-04-12 11:15:32 -0700

    Can be used to test response codes

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ from socket import *
 
 # Specify Server Address
 serverName = 'localhost'
-serverPort = 12000
+serverPort = 80
 
 # Create TCP Socket for Client
 clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -12,7 +12,10 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 
 # Recieve user input from keyboard
-sentence = "index.html"
+# Uncomment the case that is being tested
+sentence = "GET index.html HTTP1.1\r\nIf-Modified-Since: Fri, 6 Apr 2021 20:26:00 GMT\r\n" #304
+#sentence = "GET index.html"  #408
+#sentence = "GOT index.html HTTP1.1\r\nIf-Modified-Since: Fri, 6 Apr 2021 20:26:00 GMT\r\n" #400
 
 # Send! No need to specify Server Name and Server Port! Why?
 clientSocket.send(sentence.encode())
